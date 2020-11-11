87c4ca2956bfb0581733bb7b071ba759d1fe15be
Mamé <mameazad.turgut@gmail.com>
2020-11-11 21:17:42 +0100

    generate user avatar

--- client/app/handlers/signIn.js
+++ client/app/handlers/signIn.js
@@ -3,6 +3,9 @@
 export const signIn = (event) => {
 	const username = document.getElementById('user1').value;
 	const password = document.getElementById('pass1').value;
+	const login_page = document.getElementById('login-wrap');
+	const chat_forum = document.getElementById('root');
+	const avatar_list = document.getElementById('users-list');
 
 	// throw error for empty input
 	if (isEmpty(username, password)) {
@@ -17,6 +20,16 @@ export const signIn = (event) => {
 		// if user not exist in the database
 		if (!activeUser) {
 			alert(`${username.toUpperCase()}, Please sign-up, you have not any account yet`);
+		} else {
+			// render chat-forum
+			login_page.style.display = 'none';
+			chat_forum.style.display = 'block';
+
+			if (renderAvatars(data.users)) {
+				const div = document.createElement('div');
+				div.innerHTML = renderAvatars(data.users);
+				avatar_list.appendChild(div);
+			}
 		}
 	});
 };
@@ -50,4 +63,21 @@ function isUserExistInData(data, name, password) {
 	return data.find((userName) => {
 		return userName.name === name && userName.password === password;
 	});
+}
+
+// generate users avatar
+
+function renderAvatars(data) {
+	let userLists = '';
+	if (data) {
+		data.forEach((element) => {
+			userLists += `<li>
+			<img src="/img/${element.avatar}" id='${element.name}' alt="">
+			<div>
+			  <h2 >${element.name}</h2>
+			</div>
+			</li>`;
+		});
+		return userLists;
+	}
 }
