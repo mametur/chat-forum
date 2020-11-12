bbf2038389b5457e6093d504d545221bc83a9bf4
Mamé <mameazad.turgut@gmail.com>
2020-11-12 16:54:58 +0100

    add trash and edir icon

--- client/app/handlers/leave_comment.js
+++ client/app/handlers/leave_comment.js
@@ -67,7 +67,11 @@ function renderComments(data) {
         <div class="triangle"></div>
         <div class="message" id="user1comment">
        ${data.comment}
-        </div>
+		</div>
+		<div>
+		<button class="btns"><i class="far fa-trash-alt"></i></button>
+		<button class="btns" ><i class="fas fa-edit"></i></button>
+	  </div>
       `;
 
 	return comments;
